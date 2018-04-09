b703a048a3a2e325208d45334092ccf6b85b0fd9
CodeGK <codegeek1991@gmail.com>
2018-04-09 14:22:09 -0500

    codegeek91

--- routes/index.js
+++ routes/index.js
@@ -86,6 +86,7 @@ router.post('/postTest', function(req, res, next) {
         url: uri,
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0',
+          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
           cookie: '_ga=GA1.1.559186633.1506998310',
           connection: 'keep-alive',
           'upgrade-insecure-requests': '1',
